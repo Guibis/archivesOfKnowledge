b815e7cd1f27662d3a411805e668c6b086037f1c
guibis <guilhermeplima5@gmail.com>
2026-01-20 12:03:26 -0300

    feat: Implement a `/books` endpoint with file-based persistence using `books.json` and file system utilities.

--- server.js
+++ server.js
@@ -2,6 +2,30 @@ const express = require("express");
 
 const app = express();
 
+const fs = require("fs");
+
+app.use(express.json());
+
+const BOOKS_FILE = "./books.json";
+
+const readBooks = () => {
+    try {
+        const data = fs.readFileSync(BOOKS_FILE, "utf-8");
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+};
+
+const writeBooks = (books) => {
+    fs.writeFileSync(BOOKS_FILE, JSON.stringify(books, null, 2));
+};
+
+app.get("/books", (req, res) => {
+    const books = readBooks();
+    res.json(books);
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
